ee4f6208459c809a9da5ebfaec33319344f9468c
mrossant <matteorossant@gmail.com>
2021-11-09 11:44:05 -0500

    added bookmark icon to story index item

--- frontend/components/story/story_index_item.jsx
+++ frontend/components/story/story_index_item.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { 
+    postBookmark, 
+    deleteBookmark 
+} from '../../actions/bookmark_actions';
 
-const StoryIndexItem = props => {
-    return (
-        <li className="story-index-item">
-            <div className="story-author-profile">
-                <i className="fas fa-user-circle" />
-                <Link to={`/users/${props.story.author.id}`} className="story-author-name">{props.story.author.full_name}</Link>
-            </div>
-            <Link to={`/stories/${props.story.id}`} id="story-title-link">{props.story.title}</Link>
-            <span className="story-date">{props.date}</span>
-        </li>
-    )
+class StoryIndexItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bookmarked: false
+        }
+    }
+
+    render() {
+        const bookmark = this.state.bookmarked ? (
+            <i class="fas fa-bookmark" />
+        ) : (
+            <i className="far fa-bookmark" />
+        );
+
+        return (
+            <li className="story-index-item">
+                <div className="story-author-profile">
+                    <i className="fas fa-user-circle" />
+                    <Link to={`/users/${this.props.story.author.id}`} className="story-author-name">{this.props.story.author.full_name}</Link>
+                </div>
+                <Link to={`/stories/${this.props.story.id}`} id="story-title-link">{this.props.story.title}</Link>
+                <div className="story-footer">
+                    <span className="story-date">{this.props.date}</span>
+                    {bookmark}
+                </div>
+            </li>
+        )
+    }
 };
 
 export default StoryIndexItem;
